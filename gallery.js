4da9cd27a804fd1dd4fbe8135f7cac51b0e2d090
Tim <tim.wiessmeier@outlook.de>
2025-03-26 17:50:29 +0100

    bugfix

--- gallery.js
+++ gallery.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     addImage("https://img.freepik.com/free-vector/blur-pink-blue-abstract-gradient-background-vector_53876-174836.jpg?t=st=1743000647~exp=1743004247~hmac=4ffed6313587e088f9462259590431a77b4686d54029c255e1da9f7608b608cc&w=360", "background_image_01")
     addImage("https://i.pinimg.com/1200x/f7/07/2e/f7072ed7bfc4dc718142bde00dc3d06e.jpg", "background_image_pig")
     addImage("https://wallpaper-house.com/data/out/6/wallpaper2you_90760.jpg", "background_image_04");
+    addVideo("https://v55.erome.com/4503/6Jg2Fi2C/LId7Ybbx_720p.mp4", "shower_girl");
 
     images.forEach(image => {
         image.addEventListener('click', (event) => {
@@ -65,12 +66,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-function openFullscreen(img) {
-
-    const fullscreenImage = document.getElementById('fullscreenImage');
-    fullscreenImage.src = img.src;
-
+function openFullscreen(imgOrVid) {
     const modal = document.getElementById('profilePicModal');
+    const fullscreenImage = document.getElementById('fullscreenImage');
+    const fullscreenVideo = document.getElementById('fullscreenVideo');
+    const wallButton = document.getElementById("setWallpaper");
+
+    if (imgOrVid.tagName === 'IMG') {
+        wallButton.style.opacity = 100;
+        wallButton.style.display = "block"; 
+        fullscreenImage.src = imgOrVid.src;
+        fullscreenImage.style.display = 'block';
+        fullscreenVideo.style.display = 'hidden';  
+    }  else if (imgOrVid.tagName === 'VIDEO') {
+        console.log("isn video");
+        wallButton.style.opacity = 0;
+        fullscreenVideo.src = imgOrVid.src;
+        fullscreenVideo.style.display = 'block';
+        fullscreenImage.style.display = 'hidden';  
+        fullscreenVideo.play();  
+    }
+
     modal.style.display = 'block';
 }
 
@@ -81,7 +97,16 @@ function openFolder(folder){
 
 function closeModal() {
     const modal = document.getElementById('profilePicModal');
+    const fullscreenImage = document.getElementById('fullscreenImage');
+    const fullscreenVideo = document.getElementById('fullscreenVideo');
+
+    fullscreenImage.style.display = 'none';
+    fullscreenVideo.style.display = 'none';
     modal.style.display = 'none';
+    
+    // Video anhalten, wenn es abgespielt wurde
+    fullscreenVideo.pause();
+    fullscreenVideo.currentTime = 0;
 }
 
 function addImage(src, name){
@@ -104,6 +129,30 @@ function addImage(src, name){
     overlay_.appendChild(text_);
 
     img_.addEventListener('click', (event) => {
+        openFullscreen(event.target);
+    });
+}
+
+function addVideo(src, name){
+    const galleryDiv = document.getElementById("gallery");
+    const imgElement = document.createElement("div");
+    const video_ = document.createElement("video");
+    const overlay_ = document.createElement("div");
+    const text_ = document.createElement("p");
+
+    imgElement.classList.add("gallery-item");
+    text_.classList.add("overlay-text");
+    overlay_.classList.add("overlay");
+
+    video_.src = src;
+    text_.textContent = name;
+
+    galleryDiv.appendChild(imgElement);
+    imgElement.appendChild(video_);
+    imgElement.appendChild(overlay_);
+    overlay_.appendChild(text_);
+
+    video_.addEventListener('click', (event) => {
         openFullscreen(event.target);
     });
 }
